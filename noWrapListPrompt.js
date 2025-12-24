2229c5c5b67df13e7e9778a26ee4f6d117762a32
theKennethy <104810014+theKennethy@users.noreply.github.com>
2025-12-23 21:09:34 -0500

    feat: remove duplicate and empty list choices from  prompt

--- noWrapListPrompt.js
+++ noWrapListPrompt.js
@@ -1,7 +1,13 @@
 import inquirer from 'inquirer';
 import ListPrompt from 'inquirer/lib/prompts/list.js';
 
+
 class NoWrapListPrompt extends ListPrompt {
+  constructor(questions, rl, answers) {
+    super(questions, rl, answers);
+    // Force finite pagination (no infinite scroll/duplication)
+    this.paginator = new (this.paginator.constructor)(this.screen, { isInfinite: false });
+  }
   onUpKey() {
     if (this.selected > 0) {
       this.selected--;
