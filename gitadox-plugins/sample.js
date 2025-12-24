09417a55bb018719f41f3f8a636094dc5feae242
theKennethy <104810014+theKennethy@users.noreply.github.com>
2025-12-23 21:38:49 -0500

    feat: Add support for stash management and advanced git commands Note: I've followed the Conventional Commits specification to write a clear and specific commit message

---
+++ gitadox-plugins/sample.js
@@ -0,0 +1,5 @@
+export const name = 'Sample Plugin';
+export const description = 'A sample plugin for Gitadox.';
+export async function handler() {
+  console.log('Hello from the sample plugin!');
+}
